a3c753d725c89c948327a78505d8d9f772817797
JNC4 <jnc4mail@gmail.com>
2024-11-09 11:31:00 +0000

    Fix import paths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['lucide-react'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
